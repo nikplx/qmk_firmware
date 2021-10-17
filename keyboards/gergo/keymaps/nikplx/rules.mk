3cc0f2f160b679d35ef22bfbc389f0365be568e2
Niklas Paulicks <nPaulicks@gmail.com>
2021-10-17 14:47:13 +0200

    made it compile again

--- keyboards/gergo/keymaps/nikplx/rules.mk
+++ keyboards/gergo/keymaps/nikplx/rules.mk
@@ -3,16 +3,17 @@
 # Make sure you have dfu-programmer installed!
 #----------------------------------------------------------------------------
 # Firmware options
-BALLER = no 			# Enable to ball out
+BALLER = yes 			# Enable to ball out
 BALLSTEP = 20  			# Multiple in px to move, multiplied by layer number
 SCROLLSTEP = 1 			# Lines to scroll with ball
-MOUSEKEY_ENABLE = yes  		# Mouse keys, needed for baller
+MOUSEKEY_ENABLE = yes  		# Mouse keys(+4700), needed for baller
 
 #Debug options
 VERBOSE 		 = yes
 DEBUG_MATRIX_SCAN_RATE   = no
 DEBUG_BALLER 		 = no
-DEBUG_MATRIX		 = yes
+DEBUG_MATRIX		 = no
+DEBOUNCE_TYPE		 = sym_eager_pk
 
 # A bunch of stuff that you shouldn't touch unless you
 # know what you're doing.
@@ -26,8 +27,8 @@ ifneq ($(strip $(SCROLLSTEP)),)
     OPT_DEFS += -DSCROLLSTEP=$(strip $(SCROLLSTEP))
 endif
 ifeq ($(strip $(BALLER)), yes)
-	POINTING_DEVICE_ENABLE	= yes
     OPT_DEFS += -DBALLER
+	POINTING_DEVICE_ENABLE = yes
 endif
 ifeq ($(strip $(DEBUG_BALLER)), yes)
     OPT_DEFS += -DDEBUG_BALLER
@@ -35,5 +36,3 @@ endif
 ifeq ($(strip $(DEBUG_MATRIX)), yes)
     OPT_DEFS += -DDEBUG_MATRIX
 endif
-
-DEBOUNCE_TYPE= sym_pk
